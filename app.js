c46fd6424509b4920bbf9eb0e5796628900fa3d1
AriefNugroho <arief.zhang21@gmail.com>
2022-02-23 04:12:16 +0700

    add socket io server

--- app.js
+++ app.js
@@ -2,15 +2,40 @@ if (process.env.NODE_ENV !== 'production') {
   require("dotenv").config();
 }
 
-const cors = require("cors");
 const express = require("express");
+
+const { createServer } = require('http')
+const { Server } = require('socket.io')
+
 const app = express();
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*"
+  }
+})
+
+io.on('connection', (socket)=> {
+  console.log('A User has been connected.', socket.id)
+
+  socket.on('disconnect', ()=> {
+    console.log('A User has been disconnected.')
+  })
+
+  socket.on('costumEventFromClient', (payload)=> {
+    console.log('Got Payload.', payload)
+
+    
+    socket.emit('costumEventFromServer', "From Server")
+
+  })
+})
+
 const port = process.env.PORT || 3000;
 const errorHandler = require("./middleware/errorHandler");
 
 const router = require("./routes");
-
-app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,4 +44,4 @@ app.use("/", router);
 
 app.use(errorHandler);
 
-app.listen(port, (_) => console.log("Server is running on port:", port));
+httpServer.listen(port, (_) => console.log("Server is running on port:", port));
