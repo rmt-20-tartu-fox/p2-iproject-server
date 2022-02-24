01de3e781ce079e544b03455a237f3708b37ebf0
aryawdy <aryawdy16@gmail.com>
2022-02-24 10:51:06 +0700

    setup heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 const router = require("./routes/index.js");
 const errorHandler = require("./middlewares/errorHandler");
@@ -16,6 +16,6 @@ app.use("/", router);
 
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
