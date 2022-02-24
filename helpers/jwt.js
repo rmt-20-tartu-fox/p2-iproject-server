862788abfbfc9249644231f258ee0901eb282815
hilmifau <hilmi.fau05@gmail,comgit config --global user.email hilmi.fau05@gmail.com>
2022-02-24 11:07:59 +0700

    add env

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken')
-const SECRET_KEY = '!@#$%'
-
+const SECRET_KEY = process.env.SECRET_KEY
 const signToken = (payload) => {
   return jwt.sign(payload, SECRET_KEY, { expiresIn: '1d'});
 }
