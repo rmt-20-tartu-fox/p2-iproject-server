c72ecc1eb190418978def17629c153a851cd402b
ridhasidi <mulyawan.rdhsd@gmail.com>
2022-02-24 08:04:43 +0700

    improve query

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,11 +1,11 @@
 const { Op } = require("sequelize");
 const getDistanceFromLatLonInKm = require("../helpers/calculateDistance");
-const { User, Profile, Geo, Like } = require("../models/index");
+const { User, Profile, Geo } = require("../models/index");
 
 class Controller {
   static async getAllUsers(req, res, next) {
     try {
-      const { age, sex, maxDistance } = req.query;
+      const { maxAge, sex, maxDistance, page } = req.query;
       const { id } = req.currentUser;
       // get geo here
       const geo = await Geo.findOne({
@@ -14,14 +14,15 @@ class Controller {
         },
         attributes: { exclude: ["createdAt", "updatedAt"] },
       });
-
+      const limit = 1;
       let paramQuery = {
         where: {
-          id: { [Op.ne]: id },
+          [Op.and]: [{ id: { [Op.ne]: id } }],
         },
         attributes: {
           exclude: ["dateOfBirth", "password", "createdAt", "updatedAt"],
         },
+        limit: limit,
         include: [
           {
             model: Profile,
@@ -33,15 +34,20 @@ class Controller {
           },
         ],
       };
-      if (age) {
-        paramQuery.where = { age };
+      if (maxAge) {
+        paramQuery.where[Op.and].push({
+          age: { [Op.lte]: maxAge },
+        });
+      }
+      if (page && page - 1 > 0) {
+        paramQuery.offset = (page - 1) * limit;
       }
       if (sex && sex !== "both") {
         paramQuery.include[0].where = { sex };
       }
-      let users = await User.findAll(paramQuery);
+      let users = await User.findAndCountAll(paramQuery);
       let distances = [];
-      users.forEach((el) => {
+      users.rows.forEach((el) => {
         const calculation = getDistanceFromLatLonInKm(geo.latitude, geo.longitude, el.Geo.latitude, el.Geo.longitude);
         distances.push({
           id: el.id,
@@ -50,16 +56,18 @@ class Controller {
       });
 
       if (maxDistance) {
-        users.filter((el) => {
+        users.rows.filter((el) => {
           el.distance <= maxDistance;
         });
-        distance.filter((el) => {
+        distances.filter((el) => {
           el <= maxDistance;
         });
       }
+
       res.status(200).json({
+        count: users.count,
         dist: distances,
-        rows: users,
+        rows: users.rows,
       });
     } catch (error) {
       next(error);
