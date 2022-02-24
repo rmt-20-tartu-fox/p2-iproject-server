89d5db61a749329527d1db3c2de3128b759e4a95
rhezaamali <rhezaamali@gmail.com>
2022-02-24 11:11:25 +0700

    setup-deploy

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = "rahasiadong";
+const SECRET_KEY = process.env.SECRET;
 
 const payloadToToken = (payload) => {
   return jwt.sign(payload, SECRET_KEY);
