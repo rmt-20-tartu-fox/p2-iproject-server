725154067d3826195a2535b2fb8864d371011259
AriefNugroho <arief.zhang21@gmail.com>
2022-02-24 10:18:49 +0700

    cek CORS

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const express = require("express");
 
 const { createServer } = require('http')
 const { Server } = require('socket.io')
-const cors = require('cors')
+// const cors = require('cors')
 
 const app = express();
 
-app.use(cors())
+// app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const httpServer = createServer(app)
